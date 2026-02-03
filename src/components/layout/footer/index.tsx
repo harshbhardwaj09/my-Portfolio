e104a3355cfff4a5d5ef4ad6b05d20a0ede58eb0
HarshBhardwaj <bhardwajh39@gmail.com>
2026-02-03 22:43:54 +0530

    adding blog section

--- src/components/layout/footer/index.tsx
+++ src/components/layout/footer/index.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="bottom-0 my-4 mx-1 z-50 bg-gray-900 flex justify-center p-4 shadow-sm shadow-white/80">
+    <footer className="bottom-0 mt-10 mx-1 z-50 flex justify-center p-4 shadow-sm">
       Footer
     </footer>
   );
